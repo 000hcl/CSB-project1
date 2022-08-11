e5f5cae16ac899aa26db259add13a14e9900b9dd
Natalie Johansson <mnrjohansson@gmail.com>
2022-08-11 16:41:19 +0300

    added database models

--- forum/app/models.py
+++ forum/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField
+    password = models.CharField
+
+class Topic(models.Model):
+    title = models.CharField
+    text = models.CharField
+    visible = models.BooleanField(default=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Message(models.Model):
+    text = models.CharField
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    visible = models.BooleanField(default=True)
