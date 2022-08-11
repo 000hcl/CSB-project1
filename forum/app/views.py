50c53839d107a27a60b83e463be395acdb47def5
Natalie Johansson <mnrjohansson@gmail.com>
2022-08-11 17:12:02 +0300

    rudimentary index page

--- forum/app/views.py
+++ forum/app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world.")
+    return render(request, 'index.html')
+
+def register(request):
+    return HttpResponse('Register')
