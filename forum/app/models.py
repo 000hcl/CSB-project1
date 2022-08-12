f854ffa68d0e651b0c71f69103387950a1d1d9d5
Natalie Johansson <mnrjohansson@gmail.com>
2022-08-12 10:56:19 +0300

    register page works

--- forum/app/models.py
+++ forum/app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField
-    password = models.CharField
+    username = models.CharField(max_length=20, default='')
+    password = models.CharField(max_length=20, default='')
 
 class Topic(models.Model):
-    title = models.CharField
-    text = models.CharField
+    title = models.CharField(max_length=100, default='')
+    text = models.CharField(max_length=1000, default='')
     visible = models.BooleanField(default=True)
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Message(models.Model):
-    text = models.CharField
+    text = models.CharField(max_length=1000, default='')
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     visible = models.BooleanField(default=True)
--- forum/app/views.py
+++ forum/app/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Topic, Message
+
 
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def register(request):
-    return HttpResponse('Register')
+    error = ""
+    if request.method == "POST":
+        form_username = request.POST['username']
+        form_password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        db_user = User.objects.filter(username=form_username).count()
+        
+        if form_password != confirm_password:
+            error += "ERROR: Passwords don't match. "
+        
+        if len(form_password) == 0:
+            error += "ERROR: No password was given. "
+        
+        if len(form_username) == 0:
+            error += "No username was given. "
+        
+        if len(form_username) > 20:
+            error += "ERROR: Username is too long."
+        
+        if len(form_password) > 20:
+            error += "ERROR: Password is too long. "
+        
+        if (db_user >0):
+            error += "ERROR: Username is taken. "
+        
+        if error == "":
+            User.objects.create(username=form_username, password=form_password)
+            return HttpResponseRedirect("/")
+
+    return render(request, 'register.html', {'error':error})
